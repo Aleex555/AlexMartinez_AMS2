33e2117a39503611e317feb9cdfd001b3430e9bc
Aleex555 <amartinezgonzalez.cf@iesesteveterradas.cat>
2023-10-23 14:02:21 +0200

    .

--- Unity/Hasta PR1.5/Assets/Scripts/PlayerState.cs
+++ Unity/Hasta PR1.5/Assets/Scripts/PlayerState.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class PlayerState : MonoBehaviour
 {
     Animator animator;
+    public LayerMask Ground;
+    public float castDistance;
+    public float airFriction = 3.0f;
+    public float groundFriction = 3.0f;
     public float speed = 0.3f;
     private Rigidbody2D rb;
     private SpriteRenderer flip1;
+    public Vector2 boxSize;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,21 +27,25 @@ public class PlayerState : MonoBehaviour
         
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Vector2 direction = new Vector2(horizontalInput, verticalInput);
-        transform.Translate(direction * speed * Time.deltaTime);
+        //Vector2 direction = new Vector2(horizontalInput, verticalInput);
+        //transform.Translate(direction * speed * Time.deltaTime);
+        ApplyFriction();
         if (horizontalInput > 0)
         {
             flip1.flipX = false;
+            rb.AddForce(new Vector2(1000f*Time.deltaTime,0));
             animator.SetBool("Walking", true);
         }
         else if (horizontalInput<0)
         {   flip1.flipX = true;
+            rb.AddForce(new Vector2(-1000f*Time.deltaTime,0));
             animator.SetBool("Walking", true);
         }else{
             animator.SetBool("Walking",false);
         }
         if (verticalInput>0)
         {
+            rb.AddForce(new Vector2(0,1000f*Time.deltaTime));
             animator.SetBool("isJumping",true);
         }else{
             animator.SetBool("isJumping",false);
@@ -47,5 +56,20 @@ public class PlayerState : MonoBehaviour
             animator.SetBool("isCrouching",false);
         }
 
+    }
+
+    void ApplyFriction()
+    {
+        float friction = isGrounded() ? groundFriction : airFriction;
+        rb.velocity = new Vector2(rb.velocity.x * (1 - friction * Time.deltaTime), rb.velocity.y);
+    }
+
+    public bool isGrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, Ground))
+        {
+            return true;
+        }
+        return false;
     }
 }
