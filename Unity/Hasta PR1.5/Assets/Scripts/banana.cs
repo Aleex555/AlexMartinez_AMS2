7ab2e08903114d9ec4401d840431a666dc7db263
alex <amartinezgonzalez.cf@iesesteveterradas.cat>
2023-11-08 19:07:17 +0100

    .

--- Unity/Hasta PR1.5/Assets/Scripts/banana.cs
+++ Unity/Hasta PR1.5/Assets/Scripts/banana.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class banana : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D object1){
-        if (object1.CompareTag("Player")) // Puedes reemplazar "Player" con la etiqueta correcta del objeto con el que colisionas.
+    public spawner spawnerReference; // Referencia al spawner que instancia este objeto.
+
+    private void OnTriggerEnter2D(Collider2D object1)
+    {
+        if (object1.CompareTag("Player"))
         {
             Debug.Log("Gotcha");
+            if (spawnerReference != null)
+            {
+                spawnerReference.DecrementObjectCount(); // Notifica al spawner que el objeto ha sido destruido.
+            }
             Destroy(gameObject);
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
     }
 }
--- Unity/Hasta PR1.5/Assets/Scripts/spawner.cs
+++ Unity/Hasta PR1.5/Assets/Scripts/spawner.cs
@@ -7,28 +7,37 @@ public class spawner : MonoBehaviour
     float timer;
     float interval = 1f;
     public GameObject gearPreFab;
+    int numberOfObjectsToInstantiate = 0; // Variable para llevar un seguimiento del número de objetos instanciados.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Puedes inicializar numberOfObjectsToInstantiate aquí si quieres empezar con un número inicial de objetos.
     }
 
     // Update is called once per frame
-    void Update(){
+    void Update()
+    {
         timer += Time.deltaTime;
         if (timer >= interval)
         {
-
             timer = 0f;
 
-            if (gearPreFab != null)
+            if (numberOfObjectsToInstantiate < 10) // Verifica que no hayas alcanzado el límite de 10 objetos.
             {
-                float randomX = Random.Range(-10, 10f);
-                float randomY = Random.Range(0, 3);
+                float randomX = Random.Range(-10f, 10f);
+                float randomY = Random.Range(0f, 3f);
                 Vector3 randomPosition = new Vector3(randomX, randomY, 0f);
-                Instantiate(gearPreFab, transform.position, Quaternion.identity);
+                GameObject newGear = Instantiate(gearPreFab, randomPosition, Quaternion.identity);
+                numberOfObjectsToInstantiate++; // Aumenta la cuenta de objetos instanciados.
+                newGear.GetComponent<banana>().spawnerReference = this; // Asigna una referencia al spawner al objeto "banana".
             }
+        }
     }
+
+    // Método para disminuir el contador de objetos instanciados cuando "banana" destruye un objeto.
+    public void DecrementObjectCount()
+    {
+        numberOfObjectsToInstantiate--;
     }
-    
 }
